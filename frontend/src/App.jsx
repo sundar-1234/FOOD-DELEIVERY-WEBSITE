a65b6591071b4a27136ebd3e72c8f95ff77e1821
sundar-1234 <shanmugasundaram.s2005@gmail.com>
2025-02-11 12:01:34 +0530

    comitted

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './components/Navbar/navbar';
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
