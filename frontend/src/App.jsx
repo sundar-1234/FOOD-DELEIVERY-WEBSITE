5ef94936b8d3d02a90348b0919f9026576ad8e43
sundar-1234 <shanmugasundaram.s2005@gmail.com>
2025-02-13 08:51:32 +0530

    home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Cart from './pages/Cart/Cart';
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/cart' element={<Cart/>}></Route>
+        <Route path='/order' element={<PlaceOrder/>}></Route>
+      </Routes>
     </div>
   )
 }
